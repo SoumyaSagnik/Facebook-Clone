cfab99079390ac07290d584093ff7e9c97179e00
Soumya Sagnik Khanda <soumyasagnik@gmail.com>
2023-03-26 02:12:05 +0530

    Update Firebase.js

--- src/Firebase.js
+++ src/Firebase.js
@@ -3,13 +3,7 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBu23dUfljY4ra2a6qlpmRFULXtjYr5GD4",
-  authDomain: "facebook-clone-93810.firebaseapp.com",
-  projectId: "facebook-clone-93810",
-  storageBucket: "facebook-clone-93810.appspot.com",
-  messagingSenderId: "730483510633",
-  appId: "1:730483510633:web:a6a165d4942f84f10b81f8",
-  measurementId: "G-H8B10ZTTTF",
+  // config goes here
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
